c8acb274607e7e0647b5d9def9f7926e3ba10540
BrunoBrandaoFosse <bruno.brandao.fosse@gmail.com>
2025-11-07 04:31:39 -0300

    conversando com um chatbot pela inference api

--- 04_inference_api.py
+++ 04_inference_api.py
@@ -20,10 +20,28 @@
 
 # ==============================================================
 
+# from transformers import AutoTokenizer
+
+# # modelo = "mistralai/Mixtral-8x7B-Instruct-v0.1"
+# modelo = "Felladrin/Llama-68M-Chat-v1"
+
+# chat = [
+#     {"role": "user", "content": "Olá, qual é o seu nome?"},
+#     {"role": "assistant", "content": "Olá! Eu sou um modelo de IA. Como posso ajudar?"},
+#     {"role": "user", "content": "Gostaria de aprender Python. Você tem alguma dica?"},
+# ]
+
+# tokenizer_mixtral = AutoTokenizer.from_pretrained(modelo)
+# template_mixtral = tokenizer_mixtral.apply_chat_template(chat, tokenize=False, add_generation_token=True)
+
+# print(template_mixtral)
+
+# ==============================================================
+
+import requests
 from transformers import AutoTokenizer
 
-# modelo = "mistralai/Mixtral-8x7B-Instruct-v0.1"
-modelo = "Felladrin/Llama-68M-Chat-v1"
+modelo = "mistralai/Mixtral-8x7B-Instruct-v0.1"
 
 chat = [
     {"role": "user", "content": "Olá, qual é o seu nome?"},
@@ -31,7 +49,18 @@ chat = [
     {"role": "user", "content": "Gostaria de aprender Python. Você tem alguma dica?"},
 ]
 
-tokenizer_mixtral = AutoTokenizer.from_pretrained(modelo)
-template_mixtral = tokenizer_mixtral.apply_chat_template(chat, tokenize=False, add_generation_token=True)
+tokenizer = AutoTokenizer.from_pretrained(modelo)
+template = tokenizer.apply_chat_template(chat, tokenize=False, add_generation_token=True)
 
-print(template_mixtral)
+url = f"https://api-inference.huggingface.co/models/{modelo}"
+
+json = {
+    "inputs": template,
+    "options": {"use_cache": False, "wait_for_model": True},
+}
+
+response = requests.post(url, json=json)
+
+print(response.json())
+
+
